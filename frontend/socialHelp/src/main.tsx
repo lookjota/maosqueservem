76cb17d973612d5699306af2c6325b8881f2d49f
lookjota <lookjota@gmail.com>
2026-07-03 19:46:26 -0300

    config tailwindcss

--- frontend/socialHelp/src/main.tsx
+++ frontend/socialHelp/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastProvider } from './context/ToastContext.tsx'
+import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
